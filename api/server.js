3c0d7a6f1bfbe8bf0a2b3b41a6c9f50fa9593e3a
Mizuki Zenta <mizukisb@gmail.com>
2019-04-06 14:10:22 +1100

    add neccessary npm and create server file

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require("express") // import express module
+const bodyParser = require("body-parser") // import body-parser module
+
+const server = express()
+const port = 8000
+
+// set bodyParser to use
+server.use(bodyParser.json())
+
+server.use((error, req, res, next) => {
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
+
+server.listen(port, error => {
+  if (error) {
+    console.error("Server connection fail", error)
+  } else {
+    console.log(`Server started on port ${port}`)
+  }
+})
