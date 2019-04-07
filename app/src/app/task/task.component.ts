ff1a44de5319b00bdaeddd66ff3d64669301636d
Mizuki Zenta <mizukisb@gmail.com>
2019-04-07 13:56:25 +1000

    add priority badge and duedate on the task cards

--- app/src/app/task/task.component.ts
+++ app/src/app/task/task.component.ts
@@ -10,6 +10,15 @@ export class TaskComponent implements OnInit {
   // tasks data form home page
   @Input() tasks
   @Output() tasksOut = new EventEmitter()
+
+  // priority setting array
+  public prioritySetting = {
+    1: { color: "light", word: "Very low" },
+    2: { color: "medium", word: "Low" },
+    3: { color: "success", word: "Middle" },
+    4: { color: "warning", word: "High" },
+    5: { color: "danger", word: "Very high" }
+  }
   constructor(private taskService: TaskService) {}
 
   ngOnInit() {
