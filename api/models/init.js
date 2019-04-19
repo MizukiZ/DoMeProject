004f4f189e48ff5f4219267039740afe8e5280dd
Mizuki Zenta <mizukisb@gmail.com>
2019-04-19 11:04:25 +1000

    hide mlab username and password

--- api/models/init.js
+++ api/models/init.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose") // import mongoose module
 
 // use promise
 mongoose.Promise = global.Promise
+mongo_url = process.env.MLAB_URL
 
 mongoose
-  .connect("mongodb://user:password1@ds011271.mlab.com:11271/dome-database")
+  .connect(mongo_url)
   .then(() => {
     console.log("Successfully connected to mongo database")
   })
