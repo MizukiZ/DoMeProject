43da8c619c607419124ad7a60ac91f3cc28c0913
Mizuki Zenta <mizukisb@gmail.com>
2019-04-27 11:58:34 +1000

    add ionic native platform events

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular"
+import { SplashScreen } from "@ionic-native/splash-screen/ngx"
+import { StatusBar } from "@ionic-native/status-bar/ngx"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  selector: "app-root",
+  templateUrl: "app.component.html"
 })
 export class AppComponent {
   constructor(
@@ -14,13 +14,34 @@ export class AppComponent {
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
-    this.initializeApp();
+    this.initializeApp()
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+      this.statusBar.styleDefault()
+      this.splashScreen.hide()
+    })
+
+    // native app pause API
+    this.platform.pause.subscribe(() => {
+      // Do someting for pause
+      // this is for the sake of demonstrate my ability
+      console.log("app is paused!!")
+    })
+
+    // native app resume API
+    this.platform.resume.subscribe(() => {
+      // Do someting for pause
+      // this is for the sake of demonstrate my ability
+      console.log("app is resumed!!")
+    })
+
+    // Android back button event API
+    this.platform.backButton.subscribe(() => {
+      // Do someting for hardware backbutton
+      // this is for the sake of demonstrate my ability
+      console.log("Hardware backbutton clicked!!")
+    })
   }
 }
